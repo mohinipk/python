653a9e4637f0b840774577482aa71192fa4da948
SGBIT-CC <sgbit@ubuntu.ubuntu-domain>
2018-07-25 03:58:59 -0400

    checking odd

--- evenodd.py
+++ evenodd.py
@@ -1,6 +1,5 @@
 number=int(input("enter a number:"))
 print(number)
-a=number
-print("variable a is holding a number",number)
-if(number%2==0):
-  print("variable is holding even number")
+print("variable a is holding odd number",number)
+if(number%2!=0):
+  print("true")
