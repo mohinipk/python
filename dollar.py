f476a704e1ec731a33fc9d5a31cb55d2f44bb5f6
SGBIT-CC <sgbit@ubuntu.ubuntu-domain>
2018-07-25 03:48:56 -0400

    dollar file addede

---
+++ dollar.py
@@ -0,0 +1,6 @@
+rate_of_dollar=float(input("current rate of 1 dollar in Indian ruppies:"))
+print("curent rate of dollar",rate_of_dollar)
+dollar_value=float(input("enter the dollar value to convert in indain ruppies:"))
+print(dollar_value)
+Indian_value_of_dollar=rate_of_dollar*rate_of_dollar
+print("equvalent indian rupies",Indian_value_of_dollar)
