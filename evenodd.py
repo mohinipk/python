bcb7cc0e9955dd0642057c902f30bdc0ccf1716a
SGBIT-CC <sgbit@ubuntu.ubuntu-domain>
2018-07-25 04:05:26 -0400

    checking odd or even using function

--- evenodd.py
+++ evenodd.py
@@ -3,6 +3,8 @@ print(number)
 print("variable a is holding  number",number)
 def odd():
   if(number%2!=0):
-     print("true")
+     print("number is odd")
+  else:
+     print("number is even")
 
 odd()
