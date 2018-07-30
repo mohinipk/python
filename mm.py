eb5816bc07dea028d694c97dd142d7a923c17f49
SGBIT-CC <sgbit@ubuntu.ubuntu-domain>
2018-07-30 04:08:33 -0400

    ip address is extracted using grep

--- mm.py
+++ mm.py
@@ -4,8 +4,16 @@ url="http://whatismyip.host/"
 page=requests.get(url,headers={'User-Agent':"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0"})
 #print(page.text)
 soup = BeautifulSoup(page.text,'html.parser')
-ip_address_list= soup.find(class_='ipaddress').text
-print(ip_address_list)
+ip_address_list= soup.find(class_='ipaddress')
+#print(ip_address_list)
+
+m=str(ip_address_list)
+fhand=open('k.txt','w')
+fhand.write(m)
+
+
+cat k.txt | grep -oE "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" #this command works in command promt
+
 
 
 
