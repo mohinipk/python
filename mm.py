191059353bb5497cb4af83fb997faacb3acd27bb
SGBIT-CC <sgbit@ubuntu.ubuntu-domain>
2018-07-30 03:58:17 -0400

    ip address is extracted

--- mm.py
+++ mm.py
@@ -4,7 +4,7 @@ url="http://whatismyip.host/"
 page=requests.get(url,headers={'User-Agent':"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0"})
 #print(page.text)
 soup = BeautifulSoup(page.text,'html.parser')
-ip_address_list= soup.find(class_='ipaddress')
+ip_address_list= soup.find(class_='ipaddress').text
 print(ip_address_list)
 
 
